8ad3b512c4e0d3f3d2afd0c47ff606a90b5344b4
Shilllo <kimsemen0402@mail.ru>
2024-11-10 22:35:55 +0300

    Add WordCloud

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import DailyEmotionHistory from './components/DailyEmotionHistory/DailyEmotionHistory';
 import MonthlyEmotionHistory from './components/MonthlyEmotionHistory/MonthlyEmotionHistory';
 import * as React from 'react';
-
+import WordCloudCompoment from './components/WordCloud/WordCloud';
 type EmotionData = {
     [key: string]: {
         emotion: string;
@@ -11,6 +11,27 @@ type EmotionData = {
         time: string;
     }[];
 };
+// Экспорт данных
+const exportData = () => {
+    const data = localStorage.getItem('emotionData');
+    const blob = new Blob([data ?? ''], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'mood-tracker-data.json';
+    a.click();
+    URL.revokeObjectURL(url);
+};
+
+// Импорт данных
+// const importData = (file) => {
+//     const reader = new FileReader();
+//     reader.onload = (event) => {
+//         const importedData = JSON.parse(event.target.result);
+//         localStorage.setItem('mood-tracker-v2', JSON.stringify(importedData));
+//     };
+//     reader.readAsText(file);
+// };
 
 function App() {
     const [data, setData] = React.useState<EmotionData>(() => {
@@ -28,7 +49,7 @@ function App() {
     }, [data]);
     return (
         <div className="App">
-            <Header />
+            <Header exportData={exportData} />
 
             <h2
                 className="current-date"
@@ -40,6 +61,8 @@ function App() {
             <DailyEmotionHistory data={data} setData={setData} />
 
             <MonthlyEmotionHistory data={data} />
+
+            <WordCloudCompoment data={data} />
         </div>
     );
 }
---
+++ src/components/WordCloud/WordCloud.tsx
@@ -0,0 +1,181 @@
+import React, { useEffect, useRef, HTMLCanvasElement } from 'react';
+import WordCloud from 'wordcloud';
+
+interface EmotionData {
+    [key: string]: {
+        emotion: string;
+        description: string;
+        time: string;
+    }[];
+}
+
+const englishStopWords = [
+    'a',
+    'an',
+    'the',
+    'and',
+    'or',
+    'but',
+    'if',
+    'then',
+    'else',
+    'for',
+    'from',
+    'to',
+    'of',
+    'in',
+    'on',
+    'with',
+    'by',
+    'about',
+    'as',
+    'at',
+    'into',
+    'over',
+    'after',
+    'before',
+    'until',
+    'while',
+    'during',
+    'is',
+    'was',
+    'were',
+    'be',
+    'been',
+    'being',
+    'am',
+    'are',
+    'this',
+    'that',
+    'these',
+    'those',
+    'there',
+    'here',
+    'it',
+    'its',
+    'I',
+    'me',
+    'my',
+    'mine',
+    'we',
+    'us',
+    'our',
+    'ours',
+    'you',
+    'your',
+    'yours',
+    'he',
+    'him',
+    'his',
+    'she',
+    'her',
+    'hers',
+    'they',
+    'them',
+    'their',
+    'theirs',
+    'do',
+    'does',
+    'did',
+    'doing',
+    'have',
+    'has',
+    'had',
+    'having',
+    'can',
+    'could',
+    'will',
+    'would',
+    'shall',
+    'should',
+    'may',
+    'might',
+    'must',
+    'no',
+    'not',
+    'only',
+    'yes',
+    'too',
+    'very',
+    'so',
+    'just',
+    'well',
+    'like',
+    'also',
+    'more',
+    'most',
+    'many',
+    'much',
+    'some',
+    'few',
+    'any',
+    'every',
+    'all',
+    'each',
+    'either',
+    'neither',
+    'both',
+];
+
+function dataToWordCloud(data: EmotionData) {
+    const words: { [key: string]: number } = {};
+    Object.values(data).map((item) => {
+        item.map((entry) => {
+            const description = entry.description
+                .toLowerCase()
+                .replace(/[^\p{L}\s]/gu, '')
+                .replace(/\s+/g, ' ')
+                .split(' ');
+            description.forEach((word) => {
+                if (!englishStopWords.includes(word)) {
+                    if (words[word]) {
+                        words[word] += 1;
+                    } else {
+                        words[word] = 1;
+                    }
+                }
+            });
+        });
+    });
+
+    const result = [];
+    for (const [key, value] of Object.entries(words)) {
+        result.push([key, value]);
+    }
+    return result;
+}
+function WordCloudComponent({ data }: { data: EmotionData }) {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const wordData = dataToWordCloud(data);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
+            const ratio = window.devicePixelRatio || 1;
+
+            // Увеличиваем размер canvas в зависимости от плотности пикселей
+            canvas.width = 1000 * ratio;
+            canvas.height = 600 * ratio;
+            canvas.style.width = '1000px';
+            canvas.style.height = '600px';
+            if (context) {
+                context.scale(ratio, ratio);
+            }
+            WordCloud(canvasRef.current, {
+                list: wordData,
+                gridSize: Math.round(10 * ratio),
+                weightFactor: 50 * ratio,
+                fontFamily: 'Arial',
+                color: () =>
+                    '#' + Math.floor(Math.random() * 16777215).toString(16),
+                rotateRatio: 0.5,
+                backgroundColor: '#ffffff',
+            });
+        }
+    }, [wordData]);
+
+    return <canvas ref={canvasRef} style={{ alignSelf: 'center' }} />;
+}
+
+export default WordCloudComponent;
