a8ca8d8a14daa3419ba78331515ef9aaf3968b3b
Shilllo <kimsemen0402@mail.ru>
2024-11-10 14:27:31 +0300

    Add localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,47 @@ import DailyEmotionHistory from './components/DailyEmotionHistory/DailyEmotionHi
 import MonthlyEmotionHistory from './components/MonthlyEmotionHistory/MonthlyEmotionHistory';
 import * as React from 'react';
 
+type EmotionData = {
+    [key: string]: {
+        emotion: string;
+        description: string;
+        time: string;
+    }[];
+};
+
 function App() {
-    const [data, setData] = React.useState<{
-        [key: string]: {
-            emotion: string;
-            description: string;
-            time: string;
-        }[];
-    }>({
-        '10/11/2024': [
-            {
-                emotion: 'HAPPINESS',
-                description: 'Spent quality time with friends or family',
-                time: '19:00',
-            },
-            {
-                emotion: 'SADNESS',
-                description:
-                    'Had a disagreement or felt misunderstood by someone close',
-                time: '14:00',
-            },
-            {
-                emotion: 'ANXIETY',
-                description:
-                    'Faced an upcoming deadline or project at work or school',
-                time: '02:00',
-            },
-        ],
+    const [data, setData] = React.useState<EmotionData>(() => {
+        const storedData = localStorage.getItem('emotionData');
+        return storedData
+            ? JSON.parse(storedData)
+            : {
+                  '10/11/2024': [
+                      {
+                          emotion: 'HAPPINESS',
+                          description:
+                              'Spent quality time with friends or family',
+                          time: '19:00',
+                      },
+                      {
+                          emotion: 'SADNESS',
+                          description:
+                              'Had a disagreement or felt misunderstood by someone close',
+                          time: '14:00',
+                      },
+                      {
+                          emotion: 'ANXIETY',
+                          description:
+                              'Faced an upcoming deadline or project at work or school',
+                          time: '02:00',
+                      },
+                  ],
+              };
     });
+
+    // Сохранение данных в localStorage при изменении состояния
+    React.useEffect(() => {
+        localStorage.setItem('emotionData', JSON.stringify(data));
+    }, [data]);
     return (
         <div className="App">
             <Header />
