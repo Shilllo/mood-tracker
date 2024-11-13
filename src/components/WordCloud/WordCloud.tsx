0fc33f83f2ff906609575e4e8421dd763cd6b866
Shilllo <kimsemen0402@mail.ru>
2024-11-13 13:18:44 +0300

    Change default value in time-category filter

--- src/components/WordCloud/WordCloud.tsx
+++ src/components/WordCloud/WordCloud.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from "react";
-import WordCloud from "wordcloud";
-import TextField from "@mui/material/TextField";
-import MenuItem from "@mui/material/MenuItem";
-import * as React from "react";
+import { useEffect, useRef } from 'react';
+import WordCloud from 'wordcloud';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import * as React from 'react';
 
 interface EmotionData {
     [key: string]: {
@@ -13,182 +13,182 @@ interface EmotionData {
 }
 
 const englishStopWords = [
-    "a",
-    "an",
-    "the",
-    "and",
-    "or",
-    "but",
-    "if",
-    "then",
-    "else",
-    "for",
-    "from",
-    "to",
-    "of",
-    "in",
-    "on",
-    "with",
-    "by",
-    "about",
-    "as",
-    "at",
-    "into",
-    "over",
-    "after",
-    "before",
-    "until",
-    "while",
-    "during",
-    "is",
-    "was",
-    "were",
-    "be",
-    "been",
-    "being",
-    "am",
-    "are",
-    "this",
-    "that",
-    "these",
-    "those",
-    "there",
-    "here",
-    "it",
-    "its",
-    "I",
-    "me",
-    "my",
-    "mine",
-    "we",
-    "us",
-    "our",
-    "ours",
-    "you",
-    "your",
-    "yours",
-    "he",
-    "him",
-    "his",
-    "she",
-    "her",
-    "hers",
-    "they",
-    "them",
-    "their",
-    "theirs",
-    "do",
-    "does",
-    "did",
-    "doing",
-    "have",
-    "has",
-    "had",
-    "having",
-    "can",
-    "could",
-    "will",
-    "would",
-    "shall",
-    "should",
-    "may",
-    "might",
-    "must",
-    "no",
-    "not",
-    "only",
-    "yes",
-    "too",
-    "very",
-    "so",
-    "just",
-    "well",
-    "like",
-    "also",
-    "more",
-    "most",
-    "many",
-    "much",
-    "some",
-    "few",
-    "any",
-    "every",
-    "all",
-    "each",
-    "either",
-    "neither",
-    "both",
+    'a',
+    'an',
+    'the',
+    'and',
+    'or',
+    'but',
+    'if',
+    'then',
+    'else',
+    'for',
+    'from',
+    'to',
+    'of',
+    'in',
+    'on',
+    'with',
+    'by',
+    'about',
+    'as',
+    'at',
+    'into',
+    'over',
+    'after',
+    'before',
+    'until',
+    'while',
+    'during',
+    'is',
+    'was',
+    'were',
+    'be',
+    'been',
+    'being',
+    'am',
+    'are',
+    'this',
+    'that',
+    'these',
+    'those',
+    'there',
+    'here',
+    'it',
+    'its',
+    'I',
+    'me',
+    'my',
+    'mine',
+    'we',
+    'us',
+    'our',
+    'ours',
+    'you',
+    'your',
+    'yours',
+    'he',
+    'him',
+    'his',
+    'she',
+    'her',
+    'hers',
+    'they',
+    'them',
+    'their',
+    'theirs',
+    'do',
+    'does',
+    'did',
+    'doing',
+    'have',
+    'has',
+    'had',
+    'having',
+    'can',
+    'could',
+    'will',
+    'would',
+    'shall',
+    'should',
+    'may',
+    'might',
+    'must',
+    'no',
+    'not',
+    'only',
+    'yes',
+    'too',
+    'very',
+    'so',
+    'just',
+    'well',
+    'like',
+    'also',
+    'more',
+    'most',
+    'many',
+    'much',
+    'some',
+    'few',
+    'any',
+    'every',
+    'all',
+    'each',
+    'either',
+    'neither',
+    'both',
 ];
 
 const emotions = {
     positive: [
-        "HAPPINESS",
-        "JOY",
-        "EXCITEMENT",
-        "CONTENTMENT",
-        "GRATITUDE",
-        "LOVE",
-        "PRIDE",
-        "CALM",
-        "HOPE",
-        "RELIEF",
-        "AMUSEMENT",
-        "EUPHORIA",
-        "INSPIRATION",
-        "CONFIDENCE",
-        "AFFECTION",
-        "SATISFACTION",
-        "ENTHUSIASM",
+        'HAPPINESS',
+        'JOY',
+        'EXCITEMENT',
+        'CONTENTMENT',
+        'GRATITUDE',
+        'LOVE',
+        'PRIDE',
+        'CALM',
+        'HOPE',
+        'RELIEF',
+        'AMUSEMENT',
+        'EUPHORIA',
+        'INSPIRATION',
+        'CONFIDENCE',
+        'AFFECTION',
+        'SATISFACTION',
+        'ENTHUSIASM',
     ],
     negative: [
-        "SADNESS",
-        "ANGER",
-        "FEAR",
-        "ANXIETY",
-        "STRESS",
-        "DISAPPOINTMENT",
-        "FRUSTRATION",
-        "LONELINESS",
-        "GUILT",
-        "SHAME",
-        "JEALOUSY",
-        "ENVY",
-        "RESENTMENT",
-        "BOREDOM",
-        "CONFUSION",
-        "DISGUST",
-        "BITTERNESS",
-        "MELANCHOLY",
+        'SADNESS',
+        'ANGER',
+        'FEAR',
+        'ANXIETY',
+        'STRESS',
+        'DISAPPOINTMENT',
+        'FRUSTRATION',
+        'LONELINESS',
+        'GUILT',
+        'SHAME',
+        'JEALOUSY',
+        'ENVY',
+        'RESENTMENT',
+        'BOREDOM',
+        'CONFUSION',
+        'DISGUST',
+        'BITTERNESS',
+        'MELANCHOLY',
     ],
     neutral: [
-        "SURPRISE",
-        "CURIOSITY",
-        "NOSTALGIA",
-        "AMBIVALENCE",
-        "INDIFFERENCE",
-        "APATHY",
-        "ACCEPTANCE",
-        "EMPATHY",
-        "COMPASSION",
-        "ANTICIPATION",
-        "AWE",
-        "FEARLESSNESS",
-        "RESIGNATION",
-        "LONGING",
-        "SHOCK",
+        'SURPRISE',
+        'CURIOSITY',
+        'NOSTALGIA',
+        'AMBIVALENCE',
+        'INDIFFERENCE',
+        'APATHY',
+        'ACCEPTANCE',
+        'EMPATHY',
+        'COMPASSION',
+        'ANTICIPATION',
+        'AWE',
+        'FEARLESSNESS',
+        'RESIGNATION',
+        'LONGING',
+        'SHOCK',
     ],
 };
 
 const timeCategories: {
     [key: string]: string[];
 } = {
-    morning: ["05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00"], // Утро: 5:00 - 11:59
-    afternoon: ["12:00", "13:00", "14:00", "15:00", "16:00", "17:00"], // День: 12:00 - 17:59
-    evening: ["18:00", "19:00", "20:00", "21:00"], // Вечер: 18:00 - 21:59
-    night: ["22:00", "23:00", "00:00", "01:00", "02:00", "03:00", "04:00"], // Ночь: 22:00 - 4:59
+    morning: ['05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00'], // Утро: 5:00 - 11:59
+    afternoon: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00'], // День: 12:00 - 17:59
+    evening: ['18:00', '19:00', '20:00', '21:00'], // Вечер: 18:00 - 21:59
+    night: ['22:00', '23:00', '00:00', '01:00', '02:00', '03:00', '04:00'], // Ночь: 22:00 - 4:59
     all: Array.from(
         { length: 24 },
-        (_, i) => `${String(i).padStart(2, "0")}:00`
+        (_, i) => `${String(i).padStart(2, '0')}:00`,
     ),
 };
 
@@ -200,13 +200,13 @@ const periods = {
 };
 function dataToWordCloud(
     data: EmotionData,
-    emotionCategory = "all",
-    timeCategory = "all",
-    period: "day" | "week" | "month" | "year" = "day"
+    emotionCategory = 'all',
+    timeCategory = 'all',
+    period: 'day' | 'week' | 'month' | 'year' = 'day',
 ) {
     const words: { [key: string]: number } = {};
     let count = 1;
-    if (emotionCategory === "all") {
+    if (emotionCategory === 'all') {
         Object.values(data)
             .reverse()
             .map((item) => {
@@ -215,9 +215,9 @@ function dataToWordCloud(
                         if (timeCategories[timeCategory].includes(entry.time)) {
                             const description = entry.description
                                 .toLowerCase()
-                                .replace(/[^\p{L}\s]/gu, "")
-                                .replace(/\s+/g, " ")
-                                .split(" ");
+                                .replace(/[^\p{L}\s]/gu, '')
+                                .replace(/\s+/g, ' ')
+                                .split(' ');
                             description.forEach((word) => {
                                 if (!englishStopWords.includes(word)) {
                                     if (words[word]) {
@@ -233,9 +233,9 @@ function dataToWordCloud(
                 }
             });
     } else if (
-        emotionCategory === "positive" ||
-        emotionCategory === "negative" ||
-        emotionCategory === "neutral"
+        emotionCategory === 'positive' ||
+        emotionCategory === 'negative' ||
+        emotionCategory === 'neutral'
     ) {
         Object.values(data)
             .reverse()
@@ -248,9 +248,9 @@ function dataToWordCloud(
                         ) {
                             const description = entry.description
                                 .toLowerCase()
-                                .replace(/[^\p{L}\s]/gu, "")
-                                .replace(/\s+/g, " ")
-                                .split(" ");
+                                .replace(/[^\p{L}\s]/gu, '')
+                                .replace(/\s+/g, ' ')
+                                .split(' ');
                             description.forEach((word) => {
                                 if (!englishStopWords.includes(word)) {
                                     if (words[word]) {
@@ -274,29 +274,29 @@ function dataToWordCloud(
     return result;
 }
 function WordCloudComponent({ data }: { data: EmotionData }) {
-    const [emotionCategory, setEmotionCategory] = React.useState("all");
-    const [timeCategory, setTimeCategory] = React.useState("all");
+    const [emotionCategory, setEmotionCategory] = React.useState('all');
+    const [timeCategory, setTimeCategory] = React.useState('all');
     const [period, setPeriod] = React.useState<
-        "day" | "week" | "month" | "year"
-    >("day");
+        'day' | 'week' | 'month' | 'year'
+    >('week');
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const wordData = dataToWordCloud(
         data,
         emotionCategory,
         timeCategory,
-        period
+        period,
     );
     useEffect(() => {
         if (canvasRef.current) {
             const canvas = canvasRef.current;
-            const context = canvas.getContext("2d");
+            const context = canvas.getContext('2d');
             const ratio = window.devicePixelRatio || 1;
 
             // Увеличиваем размер canvas в зависимости от плотности пикселей
             canvas.width = 1000 * ratio;
             canvas.height = 600 * ratio;
-            canvas.style.width = "1000";
-            canvas.style.height = "600";
+            canvas.style.width = '1000';
+            canvas.style.height = '600';
             if (context) {
                 context.scale(ratio, ratio);
             }
@@ -304,34 +304,34 @@ function WordCloudComponent({ data }: { data: EmotionData }) {
                 list: wordData as [string, number][],
                 gridSize: Math.round(10 * ratio),
                 weightFactor: 50 * ratio,
-                fontFamily: "Arial",
+                fontFamily: 'Arial',
                 color: () =>
-                    "#" + Math.floor(Math.random() * 16777215).toString(16),
+                    '#' + Math.floor(Math.random() * 16777215).toString(16),
                 rotateRatio: 0.5,
-                backgroundColor: "#FFFFFF",
+                backgroundColor: '#FFFFFF',
             });
         }
     }, [wordData]);
 
     const handleEmotionCategoryChange = (
-        event: React.ChangeEvent<HTMLInputElement>
+        event: React.ChangeEvent<HTMLInputElement>,
     ) => {
         setEmotionCategory(event.target.value);
     };
 
     const handleTimeCategoryChange = (
-        event: React.ChangeEvent<HTMLInputElement>
+        event: React.ChangeEvent<HTMLInputElement>,
     ) => {
         setTimeCategory(event.target.value);
     };
 
     const handlePeriodChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const periodValue = event.target.value as
-            | "day"
-            | "week"
-            | "month"
-            | "year";
-        if (["day", "week", "month", "year"].includes(periodValue)) {
+            | 'day'
+            | 'week'
+            | 'month'
+            | 'year';
+        if (['day', 'week', 'month', 'year'].includes(periodValue)) {
             setPeriod(periodValue);
         } else {
             console.error(`Invalid period value: ${periodValue}`);
@@ -341,18 +341,18 @@ function WordCloudComponent({ data }: { data: EmotionData }) {
     return (
         <div
             style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
             }}
         >
             <div
                 className="filters"
                 style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "10px",
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '10px',
                 }}
             >
                 <h3>Word Cloud Filters</h3>
@@ -363,17 +363,17 @@ function WordCloudComponent({ data }: { data: EmotionData }) {
                     variant="outlined"
                     defaultValue="all"
                     sx={{
-                        width: "200px",
+                        width: '200px',
                     }}
                     value={emotionCategory}
                     onChange={handleEmotionCategoryChange}
                 >
-                    {["positive", "negative", "neutral", "all"].map(
+                    {['positive', 'negative', 'neutral', 'all'].map(
                         (option) => (
                             <MenuItem key={option} value={option}>
                                 {option}
                             </MenuItem>
-                        )
+                        ),
                     )}
                 </TextField>
                 <TextField
@@ -383,17 +383,17 @@ function WordCloudComponent({ data }: { data: EmotionData }) {
                     variant="outlined"
                     defaultValue="all"
                     sx={{
-                        width: "200px",
+                        width: '200px',
                     }}
                     value={timeCategory}
                     onChange={handleTimeCategoryChange}
                 >
-                    {["morning", "afternoon", "evening", "night", "all"].map(
+                    {['morning', 'afternoon', 'evening', 'night', 'all'].map(
                         (option) => (
                             <MenuItem key={option} value={option}>
                                 {option}
                             </MenuItem>
-                        )
+                        ),
                     )}
                 </TextField>
                 <TextField
@@ -403,12 +403,12 @@ function WordCloudComponent({ data }: { data: EmotionData }) {
                     variant="outlined"
                     defaultValue="day"
                     sx={{
-                        width: "200px",
+                        width: '200px',
                     }}
                     value={period}
                     onChange={handlePeriodChange}
                 >
-                    {["day", "week", "month", "year"].map((option) => (
+                    {['day', 'week', 'month', 'year'].map((option) => (
                         <MenuItem key={option} value={option}>
                             {option}
                         </MenuItem>
@@ -418,8 +418,8 @@ function WordCloudComponent({ data }: { data: EmotionData }) {
             <canvas
                 ref={canvasRef}
                 style={{
-                    alignSelf: "center",
-                    display: "flex",
+                    alignSelf: 'center',
+                    display: 'flex',
                 }}
             />
         </div>
