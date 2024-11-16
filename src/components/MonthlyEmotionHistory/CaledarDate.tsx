914bb408d977fb34c05de06a2747005f87a2ec86
Shilllo <kimsemen0402@mail.ru>
2024-11-16 21:07:00 +0300

    Update

--- src/components/MonthlyEmotionHistory/CaledarDate.tsx
+++ src/components/MonthlyEmotionHistory/CaledarDate.tsx
@@ -41,7 +41,7 @@ export default function CalendarDate({
             onClick={() => {
                 handleOpen(date);
             }}
-            style={{ border: `2px solid ${currentColor}` }}
+            style={{ border: `2.5px solid ${currentColor}` }}
         >
             {date + 1}
         </motion.div>
