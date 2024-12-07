fa541d4d5dc6e8fb0ab90620766248af2c3cc9fd
Shilllo <kimsemen0402@mail.ru>
2024-12-07 16:30:32 +0300

    Fix Bot.js

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -1,5 +1,7 @@
 import { Telegraf } from 'telegraf';
-const TOKEN = '7866412399:AAFYKSbU3J7z_CFekLO9HKQW9NKAHFwT1Aw';
+import 'dotenv/config';
+
+const TOKEN = process.env.TELEGRAM_TOKEN || 'ваш_токен_бота';
 const bot = new Telegraf(TOKEN);
 
 const web_link = 'https://shilllo.github.io/mood-tracker/';
@@ -14,4 +16,23 @@ bot.start((ctx) =>
     }),
 );
 
-bot.launch();
+// Настройка Webhook
+const PORT = process.env.PORT || 3000;
+const DOMAIN = process.env.RENDER_EXTERNAL_URL;
+
+if (!DOMAIN) {
+    console.error('Ошибка: DOMAIN (RENDER_EXTERNAL_URL) не задан.');
+    process.exit(1);
+}
+
+bot.launch({
+    webhook: {
+        domain: DOMAIN,
+        port: PORT,
+    },
+});
+
+console.log(`Бот запущен через Webhook на ${DOMAIN}`);
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
